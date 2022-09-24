ac5dce2eb2f0f5b85dfb87e43f8f0d42cf0a5ad0
Yongjun Park <yopark@student.42seoul.kr>
2022-09-24 23:38:34 +0900

    [Platinum V] Title: 가장 긴 팰린드롬 부분 문자열, Time: 144 ms, Memory: 119012 KB -BaekjoonHub

---
+++ 백준/Platinum/13275. 가장 긴 팰린드롬 부분 문자열/가장 긴 팰린드롬 부분 문자열.py
@@ -0,0 +1,26 @@
+import sys
+input = lambda: sys.stdin.readline().rstrip()
+# miis = lambda: map(int, input().split())
+
+def manacher(s):
+    s = "#" + "#".join(list(s)) + "#"
+    len_s = len(s)
+    ret = [0] * len_s # ret[i] : i를 중심으로 만들 수 있는 최장 Palindrome의 길이
+
+    c, r = -1, -1
+    for i in range(len_s):
+        if i <= r:
+            j = c - (i-c)
+            ret[i] = min(ret[j], r-i)
+        
+        while 0 <= i-ret[i]-1 and i+ret[i]+1 < len_s:
+            if s[i-ret[i]-1] != s[i+ret[i]+1]:
+                break
+            ret[i] += 1
+        if r < i+ret[i]:
+            c = i; r = i+ret[i]
+    return ret
+
+S = input()
+A = manacher(S)
+print(max(A))
