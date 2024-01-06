53407202c327d2130b8e1ee2b16f36c75fb1408f
Yongjun Park <yopark@student.42seoul.kr>
2024-01-07 00:57:29 +0900

    [level 1] Title: 옹알이 (2), Time: 0.22 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -2,5 +2,8 @@ const solution = (babbling) => {
     const regexp1 = /(aya|ye|woo|ma)\1+/;
     const regexp2 = /^(aya|ye|woo|ma)+$/;
 
-    return babbling.filter(word => !regexp1.test(word) && regexp2.test(word)).length;
+    return babbling.reduce((ans, word) => (
+        !regexp1.test(word) && regexp2.test(word) ? ans + 1 : ans
+    ), 0);
+    // return babbling.filter(word => !regexp1.test(word) && regexp2.test(word)).length; 보다 메모리 사용에 효율적
 }
