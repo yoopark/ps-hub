ea8e319816a26810a1ec6b6be5c528a61f653742
Yongjun Park <yopark@student.42seoul.kr>
2024-01-07 00:53:10 +0900

    [level 1] Title: 옹알이 (2), Time: 0.24 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.js
@@ -1,38 +1,6 @@
-const BABBLINGS = ['aya', 'ye', 'woo', 'ma'];
+const solution = (babbling) => {
+    const regexp1 = /(aya|ye|woo|ma)\1+/;
+    const regexp2 = /^(aya|ye|woo|ma)+$/;
 
-const canBabble = (s) => {
-    let currentIdx = 0;
-    let lastBabblingIdx = -1;
-   
-    while (currentIdx < s.length) {
-        let found = false;
-        for (let i = 0; i < BABBLINGS.length; i++) {
-            const BABBLING = BABBLINGS[i];
-            if (i === lastBabblingIdx) {
-                continue;
-            }
-            if (s.indexOf(BABBLING, currentIdx) !== currentIdx) {
-                continue;
-            }
-            found = true;
-            lastBabblingIdx = i;
-            currentIdx += BABBLING.length;
-            break;
-        }
-        if (!found) {
-            return false;
-        }
-    }
-    return true;
-}
-
-const solution = (babblings) => {
-    let answer = 0;
-    
-    for (const babbling of babblings) {
-        if (canBabble(babbling)) {
-            answer++;
-        }
-    }
-    return answer;
+    return babbling.filter(word => !regexp1.test(word) && regexp2.test(word)).length;
 }
