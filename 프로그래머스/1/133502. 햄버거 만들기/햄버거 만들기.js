08c72dce09d92cbe4684b3c4bd9aec75b28f642c
Yongjun Park <yopark@student.42seoul.kr>
2024-01-06 22:22:40 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.js
@@ -0,0 +1,26 @@
+const equal = (arr1, arr2) => {
+    if (arr1.length !== arr2.length) {
+        return false;
+    }
+    for (let i = 0; i < arr1.length; i++) {
+        if (arr1[i] !== arr2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
+
+const solution = (ingredient) => {
+    let answer = 0;
+    const stack = [];
+    
+    for (const elem of ingredient) {
+        stack.push(elem);
+        
+        while (stack.length >= 4 && equal(stack.slice(-4), [1, 2, 3, 1])) {
+            stack.splice(-4);
+            answer++;
+        }
+    }
+    return answer;
+}
