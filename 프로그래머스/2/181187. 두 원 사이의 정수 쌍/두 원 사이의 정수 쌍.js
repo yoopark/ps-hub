d5e098d0efa35b9b28ae6e6cb739df5a4dc6ed47
Yongjun Park <yopark@student.42seoul.kr>
2024-01-07 12:57:34 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 11.34 ms, Memory: 38 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.js
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.js
@@ -3,12 +3,8 @@ const solution = (r1, r2) => {
     
     for (let x = 1; x <= r2; x++) {
         const maxY = Math.floor(Math.sqrt(r2 ** 2 - x ** 2));
-        let minY = Math.ceil(Math.sqrt(r1 ** 2 - x ** 2));
+        const minY = r1 > x ? Math.ceil(Math.sqrt(r1 ** 2 - x ** 2)) : 0; // r1 >= x 로 하면 NaN 나옴. Why?
         
-        if (Number.isNaN(minY)) {
-            minY = 0;
-        }
-
         answer += maxY - minY + 1;
     }
     
