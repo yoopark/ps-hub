841c6bdcf7f9f5331e15483b20bfe362bc507f30
Yongjun Park <yopark@student.42seoul.kr>
2024-01-19 18:56:51 +0900

    [level 2] Title: 당구 연습, Time: 2.64 ms, Memory: 37.7 MB -BaekjoonHub

--- 프로그래머스/2/169198. 당구 연습/당구 연습.js
+++ 프로그래머스/2/169198. 당구 연습/당구 연습.js
@@ -3,14 +3,14 @@ const distSquared = ([x1, y1], [x2, y2]) => {
 }
 
 const solution = (m, n, startX, startY, balls) => {
-    const calculateMinDistSquared = ([ballX, ballY]) => {
-        return Math.min(...[
+    const calculateMinDistSquared = ([ballX, ballY]) => (
+        Math.min(...[
             startY !== ballY || startX < ballX ? distSquared([startX, startY], [-ballX, ballY]) : Infinity,
             startX !== ballX || startY < ballY ? distSquared([startX, startY], [ballX, -ballY]) : Infinity,
             startY !== ballY || startX > ballX ? distSquared([startX, startY], [m + m - ballX, ballY]) : Infinity,
             startX !== ballX || startY > ballY ? distSquared([startX, startY], [ballX, n + n - ballY]) : Infinity,
-        ]);
-    };
+        ])
+    );
     
     return balls.map(calculateMinDistSquared);
 }
