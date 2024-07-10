8022fa78d1b5a9d8736f7d6f9d462b03256e5307
Yongjun Park <yopark@student.42seoul.kr>
2024-07-11 00:49:27 +0900

    [Gold IV] Title: 좋은수열, Time: 104 ms, Memory: 9452 KB -BaekjoonHub

---
+++ 백준/Gold/2661. 좋은수열/좋은수열.js
@@ -0,0 +1,43 @@
+const { readFileSync } = require('fs');
+
+const input = readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const n = +input[0];
+
+const nums = [1, 2, 3];
+
+const isValid = (str) => {
+  for (let i = 1; i <= Math.floor(str.length / 2); i++) {
+    if (str.slice(-i) === str.slice(-2 * i, -i)) {
+      // 나쁜 수열
+      return false;
+    }
+  }
+  return true;
+};
+
+const solution = (n) => {
+  const bt = (str) => {
+    if (!isValid(str)) {
+      return null;
+    }
+
+    if (str.length === n) {
+      return str;
+    }
+
+    for (const num of nums) {
+      const ret = bt(str + num.toString());
+
+      if (ret !== null) {
+        return ret;
+      }
+    }
+
+    return null;
+  };
+
+  return bt('');
+};
+
+console.log(solution(n));
